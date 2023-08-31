740581e3a424266a661de4f6f5bfadfab06b073d
Aditi Jain <121606172+AditiJain2705@users.noreply.github.com>
2023-08-31 17:51:12 +0530

    Update index.jsx

--- app/Page2/index.jsx
+++ app/Page2/index.jsx
@@ -7,11 +7,11 @@ const Product = () => {
     <div className=' h-screen w-full'>
       <motion.div whileHover={{x:20,scale:1.1}} className={styles.card}>
       <img src="https://images.unsplash.com/photo-1596205250168-c3583813eea0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTIxfHxraXRjaGVuJTIwYXBwbGlhbmNlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="" />
-      <h1 className='text-white decoration-solid'>Cucine</h1>
+      <h1 className='text-white decoration-solid text-xl'>Cucine</h1>
       </motion.div>
       <motion.div whileHover={{x:-20,scale:1.1}} className={styles.card1}>
       <img src="https://images.unsplash.com/photo-1556911220-bff31c812dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fGtpdGNoZW4lMjBhcHBsaWFuY2VzfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60" alt="" />
-      <h1 className='text-white decoration-solid'>Sistemi</h1>
+      <h1 className='text-white decoration-solid text-xl'>Sistemi</h1>
       </motion.div>
       
       
